af93cb7623ee830f6eea26f4361077ef4d10feb7
Mogakamo <mogaka.amo254@gmail.com>
2021-08-08 00:41:02 +0300

    finished with the navbar

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
+import Image from 'next/image'
 
+import logo from '../../public/favicon.ico'
 
 const NavBar = () => {
     return (
@@ -9,9 +11,17 @@ const NavBar = () => {
             <AppBar position="fixed" className={classses.appBar} color="inherit">
                 <ToolBar>
                     <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={} alt="logo" height="15px" className={classes.image} />
+                        <Image src={logo} alt="logo" height="15px" className={classes.image} />
                         E FASHION
                     </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color='inherit'>
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
                 </ToolBar>
             </AppBar>
         </>
