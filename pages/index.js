822f9636cede2a1165273567c9e84f7f85f6bdc7
Mogakamo <mogaka.amo254@gmail.com>
2021-08-08 01:50:37 +0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Products, NavBar} from '../components'
+import { commerce } from '../lib/commerce'
+
+
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async() => {
+     const {data} = await commerce.products.list()
+     
+     setProducts(data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  console.log(products)
   return (
     <div>
        <NavBar/>
