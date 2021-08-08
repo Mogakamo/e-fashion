35a10853897e72b8ff672be2a3e8498ad69d999c
Mogakamo <mogaka.amo254@gmail.com>
2021-08-08 18:57:04 +0300

    added my own api key and added the cart

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import {Products, NavBar} from '../components'
-import { commerce } from '../lib/commerce'
+import commerce from '../lib/commerce'
 
+export async function getStaticProps() {
+  const { data: products } = await commerce.products.list()
+  const cart = await commerce.cart.retrieve()
+  return {
+    props: {
+      products,
+      cart
+    },
+  };
+}
 
-
-const Home = () => {
+const Home = ({products, cart}) => {
   
-  const [products, setProducts] = useState([])
-
-  const fetchProducts = async() => {
-     const {data: products } = await commerce.products.list()
-     
-     setProducts(data)
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
-
-  console.log(products)
   return (
     <div>
        <NavBar/>
-      <Products products={products} />    
+      <Products products={products} />   
+
+      console.log(cart) 
     </div>
   )
 }
 
 export default Home
+
 
